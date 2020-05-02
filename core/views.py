07238be5944d1ed2ad07bb11fbcb1f43c8f0c91c
MrEscape54 <martino.guzman@gmail.com>
2020-05-02 12:36:31 -0300

    final commit

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
-def index(request):
-    return render(request, 'core/index.html')
+class IndexView(TemplateView):
+    template_name = "core/index.html"    
+
 
